71771165baa6f7203aeec9cab51dd640d26ac21b
Eliza Voets <25413@ma-web.nl>
2022-10-23 21:33:27 +0200

    Python code added

---
+++ index.py
@@ -0,0 +1,11 @@
+# Inputs
+from time import sleep
+
+# Prints text letter for letter
+def fprint(txt):
+    for char in txt:
+        print(char, end=" ", flush= True)
+        sleep(0.7)
+        
+def firstscene():
+    
